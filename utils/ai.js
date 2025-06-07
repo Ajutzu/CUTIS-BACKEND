b6169b17ca6bd564979a91715ad16645c37da044
AJ <aeronjamescastillo6@gmail.com>
2025-06-07 15:49:42 +0800

    update

--- utils/ai.js
+++ utils/ai.js
@@ -1,8 +1,8 @@
-import FormData from 'form-data';
-import fetch from 'node-fetch';
-import cloudinary from './cloudinary.js';
-import dotenv from 'dotenv';
-import Condition from '../models/condition.js';
+import FormData from "form-data";
+import fetch from "node-fetch";
+import cloudinary from "./cloudinary.js";
+import dotenv from "dotenv";
+import Condition from "../models/condition.js";
 
 dotenv.config();
 
@@ -11,18 +11,17 @@ export const prepareImageForClassification = async (imageUrl) => {
   const arrayBuffer = await response.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
   const form = new FormData();
-  form.append('image', buffer, { filename: 'image.jpg' });
+  form.append("image", buffer, { filename: "image.jpg" });
 
   return { buffer, form };
 };
 
 export const classifyImageWithAPI = async (form) => {
-
   const headers = form.getHeaders();
-  headers['api-key'] = process.env.AI_API_KEY;
-  
+  headers["api-key"] = process.env.AI_API_KEY;
+
   const classifyResponse = await fetch(process.env.AI_ORIGIN, {
-    method: 'POST',
+    method: "POST",
     body: form,
     headers: headers,
   });
@@ -31,25 +30,39 @@ export const classifyImageWithAPI = async (form) => {
 };
 
 export const determineTargetFolder = (classification, confidence) => {
-  return confidence >= 0.9 ? classification : 'Unclassified';
+  return confidence >= 0.9 ? classification : "Unclassified";
 };
 
-export const moveImageToFolder = async (imageUrl, targetFolder, originalFilename) => {
+export const moveImageToFolder = async (
+  imageUrl,
+  targetFolder,
+  originalFilename
+) => {
   const newFolder = `Cutis/Skins/${targetFolder}`;
 
   return await cloudinary.uploader.upload(imageUrl, {
     folder: newFolder,
-    public_id: `${Date.now()}-${originalFilename}`.replace(/\s+/g, '_'),
-    resource_type: 'image'
+    public_id: `${Date.now()}-${originalFilename}`.replace(/\s+/g, "_"),
+    resource_type: "image",
   });
 };
 
 export const findConditionByClassification = async (classification) => {
   try {
-    const condition = await Condition.findOne({ name: { $regex: new RegExp(classification, 'i') } });
+    const condition = await Condition.findOne({
+      name: { $regex: new RegExp(classification, "i") },
+    });
     return condition;
   } catch (error) {
-    console.error('Error finding condition:', error);
+    console.error("Error finding condition:", error);
     return null;
+  }
+};
+
+export const confidenceLevelChecker = async (confidence, recommendation) => {
+  if (confidence >= 0.7) {
+    return recommendation;
+  } else {
+    return "Sorry, but our system detected that this image has a low classification confidence. We cannot provide a recommendation because the prediction may be inaccurate or potentially false.";
   }
 };
