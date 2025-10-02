634b648593b58c4075413d46c646b5f0955614b7
AJ <aeronjamescastillo6@gmail.com>
2025-10-02 08:56:01 +0800

    🎨 Client update: Thu, Oct  2, 2025  8:56:01 AM

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export const sendEmail = async (to, subject, html) => {
   const msg = {
     to,
-    from: process.env.EMAIL_FROM, // must match verified sender/domain
+    from: process.env.EMAIL_FROM, 
     subject,
     html,
   };
